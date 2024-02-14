1d1a941b97b61647b19c30e9068bdf307884c7e6
Matus Horvath <matus.horvath@gmail.com>
2024-02-14 21:47:36 +0100

    Pass image start address on command line

--- main.mjs
+++ main.mjs
@@ -2,6 +2,33 @@ import { Vm6502 } from './vm6502.mjs';
 import fs from 'node:fs/promises';
 import path from 'node:path';
 
+const parseCommandLine = () => {
+    try {
+        if (process.argv.length < 4 || process.argv.length > 5) {
+            throw new Error('invalid command line');
+        }
+
+        if (!/[0-9a-fA-F]{4}/.test(process.argv[2])) {
+            throw new Error('invalid command line');
+        }
+
+        const address = Number.parseInt(process.argv[2], 16);
+        const imagePath = process.argv[3];
+
+        if (process.argv[5] !== undefined || process.argv[5] !== 'dbg') {
+            throw new Error('invalid command line');
+        }
+
+        const debug = process.argv[4] === 'dbg';
+
+        return [address, imagePath, debug];
+    } catch (error) {
+        console.error(error.message);
+        console.log('Usage: node main.mjs c000 msbasic/tmp/vm6502.bin [dbg]');
+        process.exit(1);
+    }
+};
+
 const loadSymbols = async (imgPath) => {
     const parsedPath = path.parse(imgPath);
     const symbolPath = path.format({ ...parsedPath, base: undefined, ext: 'lbl' });
@@ -21,17 +48,18 @@ const loadSymbols = async (imgPath) => {
 };
 
 const main = async () => {
-    //const mem = [...await fs.readFile(process.argv[2])];
-    const img = [...await fs.readFile(process.argv[2])];
-    const symbols = await loadSymbols(process.argv[2]);
+    const [address, imagePath, debug] = parseCommandLine();
+
+    const image = [...await fs.readFile(imagePath)];
+    const symbols = await loadSymbols(imagePath);
 
     const mem = [];
-    for (let idx = 0; idx < img.length; idx++) {
-        mem[0xc000 + idx] = img[idx]; // TODO remove hardcoded msbasic address
+    for (let idx = 0; idx < image.length; idx++) {
+        mem[address + idx] = image[idx];
     }
 
     const vm = new Vm6502(mem, symbols);
-    //vm.trace = true;
+    vm.trace = debug;
 
     vm.run();
 };
