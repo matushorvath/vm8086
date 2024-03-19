56aaf5637fa0fd9eba26a4eea4b8567d1a9214de
Matus Horvath <matus.horvath@gmail.com>
2024-03-19 02:45:37 +0100

    Smoke test, crash test

---
+++ test/invalid_opcode/invalid_opcode.asm
@@ -0,0 +1,7 @@
+cpu 8086
+
+org 0x00000
+
+section .text start=0xffff0             ; boot
+    db  0x0f                            ; invalid opcode
+    hlt
---
+++ test/smoke/smoke.asm
@@ -0,0 +1,7 @@
+cpu 8086
+
+org 0x00000
+
+section .text start=0xffff0              ; boot
+    nop
+    hlt
