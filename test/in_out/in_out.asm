66d81acd801065e3975f52437a318f5a676fb091
Matus Horvath <matus.horvath@gmail.com>
2024-03-20 02:29:52 +0100

    Very basic OUT test

--- test/in_out/in_out.asm
+++ test/in_out/in_out.asm
@@ -11,25 +11,21 @@ section .text start=0x80000
 handle_int3:                            ; INT 3 handler
     out 0x42, al
 
-    ; push and pop each register
-    inc ax
-    push ax
-    push ax
-    pop ax
+    ; TODO when we have mov immediate, output some interesting numbers
+    ; TODO use 16-bit numbers with high byte set
 
+    ; TODO OUT AL, IMMED8
+    ; TODO OUT AX, IMMED8
+    ; TODO OUT AL, DX
+    ; TODO OUT AX, DX
 
-    out 0x42, al
+    ; TODO IN AL, IMMED8
+    ; TODO IN AX, IMMED8
+    ; TODO IN AL, DX
+    ; TODO IN AX, DX
 
-    ; everything is messed up, our work here is done
     hlt
 
 
 section boot start=0xffff0              ; boot
     int3
-
-
-    db  execute_out_al_immediate_b, 0, 0                # 0xe6 OUT AL, IMMED8
-    db  execute_out_ax_immediate_b, 0, 0                # 0xe7 OUT AX, IMMED8
-
-    db  execute_out_al_dx, 0, 0                         # 0xee OUT AL, DX
-    db  execute_out_ax_dx, 0, 0                         # 0xef OUT AX, DX
