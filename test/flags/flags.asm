76c36adda13b20ff67d5344c785d87f0aba90a42
Matus Horvath <matus.horvath@gmail.com>
2024-03-22 16:32:59 +0100

    LAHF/SAHF

--- test/flags/flags.asm
+++ test/flags/flags.asm
@@ -1,3 +1,5 @@
+; TODO test PUSHF/POPF/LAHF/SAHF with flags that can only be set through stack
+
 cpu 8086
 
 
@@ -12,33 +14,81 @@ handle_int3:                            ; INT 3 handler
     out 0x42, al
 
     ; all flags are cleared after boot
+    pushf
 
+    ; set/reset individual flags with instructions
     stc
+    pushf
+    lahf
     out 0x42, al
 
     sti
+    pushf
+    lahf
     out 0x42, al
 
     std
+    pushf
+    lahf
     out 0x42, al
 
     clc
+    pushf
+    lahf
     out 0x42, al
 
     cli
+    pushf
+    lahf
     out 0x42, al
 
     cld
+    pushf
+    lahf
     out 0x42, al
 
     cmc
+    pushf
+    lahf
     out 0x42, al
 
     cmc
+    pushf
+    lahf
     out 0x42, al
 
-    ; TODO PUSHF after every change
-    ; TODO POPF to set each flag individually
+    ; set all flags, then pop them from stack and verify
+    mov dx, 0xffff
+    push dx
+    popf
+    out 0x42, al
+
+    popf
+    out 0x42, al
+
+    popf
+    out 0x42, al
+
+    popf
+    out 0x42, al
+
+    popf
+    out 0x42, al
+
+    popf
+    out 0x42, al
+
+    popf
+    out 0x42, al
+
+    popf
+    out 0x42, al
+
+    popf
+    out 0x42, al
+
+    popf
+    out 0x42, al
 
     hlt
 
