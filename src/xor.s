e604bd95f13dee30aed9727721e5b16a1e71e485
Matus Horvath <matus.horvath@gmail.com>
2024-03-23 18:50:05 +0100

    Refactor 8-bit XOR

--- src/xor.s
+++ src/xor.s
@@ -33,45 +33,28 @@
 
 ##########
 execute_xor_b:
-.FRAME loc_type_src, loc_addr_src, loc_type_dst, loc_addr_dst; tmp, a_bits, b_bits, bit, res
-    arb -5
+.FRAME loc_type_src, loc_addr_src, loc_type_dst, loc_addr_dst; a, b, res
+    arb -3
 
     # Read the source value
     add [rb + loc_type_src], 0, [rb - 1]
     add [rb + loc_addr_src], 0, [rb - 2]
     arb -2
     call read_location_b
-
-    # Find source value bits
-    mul [rb - 4], 8, [rb + a_bits]
-    add bits, [rb + a_bits], [rb + a_bits]
+    add [rb - 4], 0, [rb + a]
 
     # Read the destination value
     add [rb + loc_type_dst], 0, [rb - 1]
     add [rb + loc_addr_dst], 0, [rb - 2]
     arb -2
     call read_location_b
+    add [rb - 4], 0, [rb + b]
 
-    # Find destination value bits
-    mul [rb - 4], 8, [rb + b_bits]
-    add bits, [rb + b_bits], [rb + b_bits]
-
-    # Process individual bits and build the output
-    add 0, 0, [rb + res]
-    add 8, 0, [rb + bit]
-
-execute_xor_b_loop:
-    add [rb + bit], -1, [rb + bit]
-
-    mul [rb + res], 2, [rb + res]                           # res << 1
-
-    add [rb + a_bits], [rb + bit], [ip + 5]
-    add [rb + b_bits], [rb + bit], [ip + 2]
-    eq  [0], [0], [rb + tmp]                                # if both bits are equal, tmp is 1
-    eq  [rb + tmp], 0, [rb + tmp]                           # tmp = ~tmp
-    add [rb + res], [rb + tmp], [rb + res]                  # res += bit n
-
-    jnz [rb + bit], execute_xor_b_loop
+    add [rb + a], 0, [rb - 1]
+    add [rb + b], 0, [rb - 2]
+    arb -2
+    call xor_b
+    add [rb - 4], 0, [rb + res]
 
     # Update flags
     add 0, 0, [flag_carry]
@@ -91,8 +74,42 @@ execute_xor_b_loop:
     arb -3
     call write_location_b
 
-    arb 5
+    arb 3
     ret 4
+.ENDFRAME
+
+##########
+xor_b:
+.FRAME a, b; res, tmp, a_bits, b_bits, bit
+    arb -5
+
+    # Find source value bits
+    mul [rb + a], 8, [rb + a_bits]
+    add bits, [rb + a_bits], [rb + a_bits]
+
+    # Find destination value bits
+    mul [rb + b], 8, [rb + b_bits]
+    add bits, [rb + b_bits], [rb + b_bits]
+
+    # Process individual bits and build the output
+    add 0, 0, [rb + res]
+    add 8, 0, [rb + bit]
+
+execute_xor_b_loop:
+    add [rb + bit], -1, [rb + bit]
+
+    mul [rb + res], 2, [rb + res]                           # res << 1
+
+    add [rb + a_bits], [rb + bit], [ip + 5]
+    add [rb + b_bits], [rb + bit], [ip + 2]
+    eq  [0], [0], [rb + tmp]                                # if both bits are equal, tmp is 1
+    eq  [rb + tmp], 0, [rb + tmp]                           # tmp = ~tmp
+    add [rb + res], [rb + tmp], [rb + res]                  # res += bit n
+
+    jnz [rb + bit], execute_xor_b_loop
+
+    arb 5
+    ret 2
 .ENDFRAME
 
 .EOF
