2554ff097baa295325ae891a1a3766cb86a05e1e
Matus Horvath <matus.horvath@gmail.com>
2024-03-27 01:06:18 +0100

    Move out old tracing code

--- src-6502/trace.s
+++ src-6502/trace.s
@@ -13,6 +13,74 @@
 .IMPORT print_num_radix
 .IMPORT print_str
 
+
+
+
+
+
+
+##########
+execute:
+.FRAME tmp, op
+    arb -2
+
+execute_loop:
+# TODO tracing
+#    # Skip tracing if disabled
+#    jz  [binary_enable_tracing], execute_tracing_done
+#
+#    # If the [binary_enable_tracing] flag is positive, we use it as an address
+#    # starting from where we should turn on tracing
+#    eq  [binary_enable_tracing], [reg_ipxxx], [rb + tmp]
+#    jz  [rb + tmp], execute_tracing_different_address
+#
+#    # Address match, turn on tracing
+#    add -1, 0, [binary_enable_tracing]
+#
+#execute_tracing_different_address:
+#    # Print trace if enabled
+#    eq  [binary_enable_tracing], -1, [rb + tmp]
+#    jz  [rb + tmp], execute_tracing_done
+#
+#    call print_trace
+#
+#execute_tracing_done:
+#    # Call the callback if enabled
+#    jz  [binary_vm_callback], execute_callback_done
+#
+#    call [binary_vm_callback]
+#    jnz [rb - 2], execute_callback_done
+#
+#    # Callback returned 0, halt
+#    jz  0, execute_hlt
+#
+#execute_callback_done:
+    # Handle prefixes; first check if they were consumed (prefix_valid == 0)
+    jz  [prefix_valid], execute_prefix_done
+
+    # Decrease prefix lifetime
+    add [prefix_valid], -1, [prefix_valid]
+
+    # If now prefix_valid == 0, we have just used the prefixes, reset them to defaults
+    jnz [prefix_valid], execute_prefix_done
+
+    add reg_ds, 0, [ds_segment_prefix]
+    add reg_ss, 0, [ss_segment_prefix]
+    add 0, 0, [rep_prefix]
+
+execute_prefix_done:
+    # Read op code
+    call read_cs_ip_b
+    add [rb - 2], 0, [rb + op]
+
+
+
+
+
+
+
+
+
 ##########
 print_trace:
 .FRAME opcode, opname, length, idx, tmp
