6db2d17e10c855226c5de3b129367ee10ff79288
Matus Horvath <matus.horvath@gmail.com>
2024-03-30 04:21:18 +0100

    More tests almost validated

--- test/prefix/prefix.asm
+++ test/prefix/prefix.asm
@@ -15,13 +15,21 @@
 %endmacro
 
 
+ds_seg      equ 0x2000
+ss_seg      equ 0x3000
+es_seg      equ 0x4000
+
+
 section .text
     jmp start
 
     ; make sure the data_read starts at an interesting address, not 0
     dw  17 dup 0x0000
+
 data_read:
     dw  0x1111
+
+    ; we can't write to .text since it's in ROM, but we can write to other segments
 data_write:
     dw  0x0000
 
@@ -30,28 +38,29 @@ start:
     out 0x80, al
 
     ; some basic tests
+    ; TODO HW lock freezes on bochs, investigate
     lock nop
     repz nop
     repnz nop
 
+    ; set up ds
+    mov dx, ds_seg
+    mov ds, dx
+
     ; check segment prefix with a segment register
     mov es, [cs:data_read]
-    mov [cs:data_write], es
+    mov [ds:data_write], es
 
     ; make the value visible in dump without depending on segment prefixes
-    mov dx, cs
-    mov ds, dx
     mov dx, [data_write]
 
     dump_state
     out 0x81, al
 
-    ; set up segments (cs is already set up by jmp)
-    mov ax, 0x2000
-    mov ds, ax
-    mov ax, 0x3000
+    ; set up other segments
+    mov ax, ss_seg
     mov ss, ax
-    mov ax, 0x4000
+    mov ax, es_seg
     mov es, ax
 
     ; set up different data in each segment
@@ -104,8 +113,6 @@ start:
     mov bx, data_write
     mov bp, data_write
 
-    mov ax, 0x2345
-    mov [cs:bp], ax
     mov ax, 0x3456
     mov [ds:bp], ax
     mov ax, 0x4567
@@ -114,7 +121,6 @@ start:
     mov [es:data_write], ax
 
     clear_registers
-    mov ax, [cs:data_write]             ; make the values visible in dump
     mov bx, [ds:data_write]
     mov cx, [ss:data_write]
     mov dx, [es:data_write]
@@ -137,12 +143,14 @@ start:
     ; check that combinations of prefixes work correctly
     clear_registers
 
+    ; TODO HW rep lock freezes on bochs, investigate
     lock mov bx, [es:data_read]
     mov cx, [data_read]
 
     rep mov dx, [es:data_read]
     mov sp, [data_read]
 
+    ; TODO HW rep lock mov freezes on bochs, investigate
     rep lock mov si, [es:data_read]
     mov di, [data_read]
 
@@ -172,11 +180,9 @@ start:
 
     mov [bp], byte 0x13
     mov [ds:bp], byte 0x24
-    mov [cs:bp], byte 0x35
 
     mov al, [bp]                        ; make the values visible in dump
     mov bl, [ds:bp]
-    mov cl, [cs:bp]
 
     dump_state
     out 0x8b, al
@@ -187,11 +193,9 @@ start:
 
     mov [bp], word 0x1357
     mov [ds:bp], word 0x2468
-    mov [cs:bp], word 0x3579
 
     mov ax, [bp]                        ; make the values visible in dump
     mov bx, [ds:bp]
-    mov cx, [cs:bp]
 
     dump_state
     out 0x8c, al
