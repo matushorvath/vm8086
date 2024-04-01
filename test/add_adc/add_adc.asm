601301d65f28a8658180303e2e72cdedd730d264
Matus Horvath <matus.horvath@gmail.com>
2024-04-01 21:20:31 +0200

    ADC/ADD 8-bit coverage

--- test/add_adc/add_adc.asm
+++ test/add_adc/add_adc.asm
@@ -16,7 +16,12 @@ section .text
     mov ds, ax
 
     mark 0x80
-
 %include "adc_nc_8.inc"
+    mark 0x81
+%include "adc_c_8.inc"
+    mark 0x82
+%include "add_8.inc"
+    mark 0x83
+%include "add_adc_8_mem.inc"
 
     call power_off
