356f8b962e3c32bae16e66108297ed6cc0214810
Matus Horvath <matus.horvath@gmail.com>
2024-04-03 00:54:49 +0200

    Failing SBB/SUB 8-bit test

---
+++ test/sub_sbb_cmp/sub_sbb_cmp.asm
@@ -0,0 +1,38 @@
+; TODO SBB/SUB AL
+; TODO SBB/SUB AX
+; TODO SBB/SUB from group_immed (8 and 16 bit)
+
+%include "common.inc"
+
+
+section data_segment start=0x10000 nobits
+    resw 29
+test_ds_8:
+    resw 263
+test_ds_16:
+    resw 1
+
+
+section .text
+    dump_state
+
+    mov ax, 0x1000
+    mov ds, ax
+
+    mark 0x80
+%include "sbb_nc_8.inc"
+;     mark 0x81
+; %include "sbb_c_8.inc"
+    mark 0x82
+%include "sub_8.inc"
+;     mark 0x83
+; %include "sub_sbb_8_mem.inc"
+
+;     mark 0x90
+; %include "sbb_nc_16.inc"
+;     mark 0x91
+; %include "sbb_c_16.inc"
+;     mark 0x92
+; %include "sub_16.inc"
+
+    call power_off
