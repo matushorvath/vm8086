511daea09671f6b8161809414512456bc9cdfe6f
Matus Horvath <matus.horvath@gmail.com>
2024-04-06 21:28:08 +0200

    Some tests are green for SUB/SBB

--- test/sub_sbb_cmp/sub_sbb_cmp.asm
+++ test/sub_sbb_cmp/sub_sbb_cmp.asm
@@ -19,14 +19,14 @@ section .text
     mov ax, 0x1000
     mov ds, ax
 
-;     mark 0x80
-; %include "sbb_nc_8.inc"
-;     mark 0x81
-; %include "sbb_c_8.inc"
+    mark 0x80
+%include "sbb_nc_8.inc"
+    mark 0x81
+%include "sbb_c_8.inc"
     mark 0x82
 %include "sub_8.inc"
-;     mark 0x83
-; %include "sub_sbb_8_mem.inc"
+    mark 0x83
+%include "sub_sbb_8_mem.inc"
 
 ;     mark 0x90
 ; %include "sbb_nc_16.inc"
