6434dd22c665294ec6f0403241dc5b8643b59156
Matus Horvath <matus.horvath@gmail.com>
2024-04-11 02:52:20 +0200

    Refactor group shift to use function ptrs

--- src/group_shift.s
+++ src/group_shift.s
@@ -51,6 +51,8 @@
 # 0xd2 <op> REG8/MEM8, CL
 # 0xd3 <op> REG16/MEM16, CL
 
+# TODO use this as pattern for handling other groups
+
 ##########
 .FRAME op, loc_type, loc_addr; table
     # Function with multiple entry points
@@ -73,210 +75,77 @@ execute_shift_cl_b:
 execute_shift_cl_w:
     arb -1
     add shift_cl_w_table, 0, [rb + table]
-    jz  0, execute_shift
 
 execute_shift:
-    # Prepare the arguments on stack
+    # Determine which function to call
+    add [rb + table], [rb + op], [ip + 1]
+    add [0], 0, [execute_shift_function]
+
+    # Call the function
     add [rb + loc_type], 0, [rb - 1]
     add [rb + loc_addr], 0, [rb - 2]
-
-    # Execute the operation
-    add [rb + table], [rb + op], [ip + 2]
-    jz  0, [0]
-    # TODO call, not jz
-
-execute_shift_invalid_op:
-    add invalid_op_message, 0, [rb - 1]
-    arb -1
-    call report_error
-
-execute_shift_rol_1_b:
     arb -2
-    call execute_rol_1_b
-    jz  0, execute_shift_end
+    call [execute_shift_function]
 
-execute_shift_ror_1_b:
-    arb -2
-    call execute_ror_1_b
-    jz  0, execute_shift_end
-
-execute_shift_rcl_1_b:
-    arb -2
-    call execute_rcl_1_b
-    jz  0, execute_shift_end
-
-execute_shift_rcr_1_b:
-    arb -2
-    call execute_rcr_1_b
-    jz  0, execute_shift_end
-
-execute_shift_shl_1_b:
-    arb -2
-    call execute_shl_1_b
-    jz  0, execute_shift_end
-
-execute_shift_shr_1_b:
-    arb -2
-    call execute_shr_1_b
-    jz  0, execute_shift_end
-
-execute_shift_sar_1_b:
-    arb -2
-    call execute_sar_1_b
-    jz  0, execute_shift_end
-
-execute_shift_rol_1_w:
-    arb -2
-    call execute_rol_1_w
-    jz  0, execute_shift_end
-
-execute_shift_ror_1_w:
-    arb -2
-    call execute_ror_1_w
-    jz  0, execute_shift_end
-
-execute_shift_rcl_1_w:
-    arb -2
-    call execute_rcl_1_w
-    jz  0, execute_shift_end
-
-execute_shift_rcr_1_w:
-    arb -2
-    call execute_rcr_1_w
-    jz  0, execute_shift_end
-
-execute_shift_shl_1_w:
-    arb -2
-    call execute_shl_1_w
-    jz  0, execute_shift_end
-
-execute_shift_shr_1_w:
-    arb -2
-    call execute_shr_1_w
-    jz  0, execute_shift_end
-
-execute_shift_sar_1_w:
-    arb -2
-    call execute_sar_1_w
-    jz  0, execute_shift_end
-
-execute_shift_rol_cl_b:
-    arb -2
-    call execute_rol_cl_b
-    jz  0, execute_shift_end
-
-execute_shift_ror_cl_b:
-    arb -2
-    call execute_ror_cl_b
-    jz  0, execute_shift_end
-
-execute_shift_rcl_cl_b:
-    arb -2
-    call execute_rcl_cl_b
-    jz  0, execute_shift_end
-
-execute_shift_rcr_cl_b:
-    arb -2
-    call execute_rcr_cl_b
-    jz  0, execute_shift_end
-
-execute_shift_shl_cl_b:
-    arb -2
-    call execute_shl_cl_b
-    jz  0, execute_shift_end
-
-execute_shift_shr_cl_b:
-    arb -2
-    call execute_shr_cl_b
-    jz  0, execute_shift_end
-
-execute_shift_sar_cl_b:
-    arb -2
-    call execute_sar_cl_b
-    jz  0, execute_shift_end
-
-execute_shift_rol_cl_w:
-    arb -2
-    call execute_rol_cl_w
-    jz  0, execute_shift_end
-
-execute_shift_ror_cl_w:
-    arb -2
-    call execute_ror_cl_w
-    jz  0, execute_shift_end
-
-execute_shift_rcl_cl_w:
-    arb -2
-    call execute_rcl_cl_w
-    jz  0, execute_shift_end
-
-execute_shift_rcr_cl_w:
-    arb -2
-    call execute_rcr_cl_w
-    jz  0, execute_shift_end
-
-execute_shift_shl_cl_w:
-    arb -2
-    call execute_shl_cl_w
-    jz  0, execute_shift_end
-
-execute_shift_shr_cl_w:
-    arb -2
-    call execute_shr_cl_w
-    jz  0, execute_shift_end
-
-execute_shift_sar_cl_w:
-    arb -2
-    call execute_sar_cl_w
-
-execute_shift_end:
     arb 1
     ret 3
+
+execute_shift_function:
+    # Global variable to avoid issues with updated rb
+    db  0
 .ENDFRAME
 
 ##########
-# Map each OP value to the label that handles it
+# Map each OP value to the function that handles it
 shift_1_b_table:
-    db  execute_shift_rol_1_b
-    db  execute_shift_ror_1_b
-    db  execute_shift_rcl_1_b
-    db  execute_shift_rcr_1_b
-    db  execute_shift_shl_1_b
-    db  execute_shift_shr_1_b
-    db  execute_shift_invalid_op
-    db  execute_shift_sar_1_b
+    db  execute_rol_1_b
+    db  execute_ror_1_b
+    db  execute_rcl_1_b
+    db  execute_rcr_1_b
+    db  execute_shl_1_b
+    db  execute_shr_1_b
+    db  invalid_shift_op
+    db  execute_sar_1_b
 
 shift_cl_b_table:
-    db  execute_shift_rol_cl_b
-    db  execute_shift_ror_cl_b
-    db  execute_shift_rcl_cl_b
-    db  execute_shift_rcr_cl_b
-    db  execute_shift_shl_cl_b
-    db  execute_shift_shr_cl_b
-    db  execute_shift_invalid_op
-    db  execute_shift_sar_cl_b
+    db  execute_rol_cl_b
+    db  execute_ror_cl_b
+    db  execute_rcl_cl_b
+    db  execute_rcr_cl_b
+    db  execute_shl_cl_b
+    db  execute_shr_cl_b
+    db  invalid_shift_op
+    db  execute_sar_cl_b
 
 shift_1_w_table:
-    db  execute_shift_rol_1_w
-    db  execute_shift_ror_1_w
-    db  execute_shift_rcl_1_w
-    db  execute_shift_rcr_1_w
-    db  execute_shift_shl_1_w
-    db  execute_shift_shr_1_w
-    db  execute_shift_invalid_op
-    db  execute_shift_sar_1_w
+    db  execute_rol_1_w
+    db  execute_ror_1_w
+    db  execute_rcl_1_w
+    db  execute_rcr_1_w
+    db  execute_shl_1_w
+    db  execute_shr_1_w
+    db  invalid_shift_op
+    db  execute_sar_1_w
 
 shift_cl_w_table:
-    db  execute_shift_rol_cl_w
-    db  execute_shift_ror_cl_w
-    db  execute_shift_rcl_cl_w
-    db  execute_shift_rcr_cl_w
-    db  execute_shift_shl_cl_w
-    db  execute_shift_shr_cl_w
-    db  execute_shift_invalid_op
-    db  execute_shift_sar_cl_w
+    db  execute_rol_cl_w
+    db  execute_ror_cl_w
+    db  execute_rcl_cl_w
+    db  execute_rcr_cl_w
+    db  execute_shl_cl_w
+    db  execute_shr_cl_w
+    db  invalid_shift_op
+    db  execute_sar_cl_w
 
-invalid_op_message:
+##########
+invalid_shift_op:
+.FRAME loc_type, loc_addr;
+    add invalid_shift_op_message, 0, [rb - 1]
+    arb -1
+    call report_error
+
+invalid_shift_op_message:
     db  "invalid group shift operation", 0
+.ENDFRAME
 
 .EOF
