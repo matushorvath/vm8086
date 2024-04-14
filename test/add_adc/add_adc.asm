eb5a0e9efd6a5d1dddfcdce597a6ef32f1c03f2e
Matus Horvath <matus.horvath@gmail.com>
2024-04-14 15:25:00 +0200

    Refactor ADD tests

--- test/add_adc/add_adc.asm
+++ test/add_adc/add_adc.asm
@@ -5,59 +5,59 @@
 %include "common.inc"
 
 
-section data_segment start=0x10000 nobits
-    resw 29
-test_ds_8:
-    resw 263
-test_ds_16:
-    resw 1
+; section data_segment start=0x10000 nobits
+;     resw 29
+; test_ds_8:
+;     resw 263
+; test_ds_16:
+;     resw 1
 
 
 section .text
-    dump_state
+     dump_state
 
-    mov ax, 0x1000
-    mov ds, ax
+;     mov ax, 0x1000
+;     mov ds, ax
 
-    mark 0x80
-%include "adc_nc_8.inc"
-    mark 0x81
-%include "adc_c_8.inc"
-    mark 0x82
-%include "add_8.inc"
-    mark 0x83
-%include "add_adc_8_mem.inc"
+;     mark 0x80
+; %include "adc_nc_8.inc"
+;     mark 0x81
+; %include "adc_c_8.inc"
+;     mark 0x82
+; %include "add_8.inc"
+;     mark 0x83
+; %include "add_adc_8_mem.inc"
 
-    mark 0x90
-%include "adc_nc_16.inc"
-    mark 0x91
-%include "adc_c_16.inc"
-    mark 0x92
-%include "add_16.inc"
+;     mark 0x90
+; %include "adc_nc_16.inc"
+;     mark 0x91
+; %include "adc_c_16.inc"
+;     mark 0x92
+; %include "add_16.inc"
 
 ; TODO select specific numbers to test + semi-random selection, don't test the whole range
 %include "add_adc_8_all.inc"
-%include "add_adc_16_all.inc"
+; %include "add_adc_16_all.inc"
 
-    call power_off
+     call power_off
 
 
-section .data start=data_addr
+; section .data start=data_addr
 
-parity:
-    db  1, 0, 0, 1, 0, 1, 1, 0, 0, 1, 1, 0, 1, 0, 0, 1
-    db  0, 1, 1, 0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 1, 0
-    db  0, 1, 1, 0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 1, 0
-    db  1, 0, 0, 1, 0, 1, 1, 0, 0, 1, 1, 0, 1, 0, 0, 1
-    db  0, 1, 1, 0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 1, 0
-    db  1, 0, 0, 1, 0, 1, 1, 0, 0, 1, 1, 0, 1, 0, 0, 1
-    db  1, 0, 0, 1, 0, 1, 1, 0, 0, 1, 1, 0, 1, 0, 0, 1
-    db  0, 1, 1, 0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 1, 0
-    db  0, 1, 1, 0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 1, 0
-    db  1, 0, 0, 1, 0, 1, 1, 0, 0, 1, 1, 0, 1, 0, 0, 1
-    db  1, 0, 0, 1, 0, 1, 1, 0, 0, 1, 1, 0, 1, 0, 0, 1
-    db  0, 1, 1, 0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 1, 0
-    db  1, 0, 0, 1, 0, 1, 1, 0, 0, 1, 1, 0, 1, 0, 0, 1
-    db  0, 1, 1, 0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 1, 0
-    db  0, 1, 1, 0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 1, 0
-    db  1, 0, 0, 1, 0, 1, 1, 0, 0, 1, 1, 0, 1, 0, 0, 1
+; parity:
+;     db  1, 0, 0, 1, 0, 1, 1, 0, 0, 1, 1, 0, 1, 0, 0, 1
+;     db  0, 1, 1, 0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 1, 0
+;     db  0, 1, 1, 0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 1, 0
+;     db  1, 0, 0, 1, 0, 1, 1, 0, 0, 1, 1, 0, 1, 0, 0, 1
+;     db  0, 1, 1, 0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 1, 0
+;     db  1, 0, 0, 1, 0, 1, 1, 0, 0, 1, 1, 0, 1, 0, 0, 1
+;     db  1, 0, 0, 1, 0, 1, 1, 0, 0, 1, 1, 0, 1, 0, 0, 1
+;     db  0, 1, 1, 0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 1, 0
+;     db  0, 1, 1, 0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 1, 0
+;     db  1, 0, 0, 1, 0, 1, 1, 0, 0, 1, 1, 0, 1, 0, 0, 1
+;     db  1, 0, 0, 1, 0, 1, 1, 0, 0, 1, 1, 0, 1, 0, 0, 1
+;     db  0, 1, 1, 0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 1, 0
+;     db  1, 0, 0, 1, 0, 1, 1, 0, 0, 1, 1, 0, 1, 0, 0, 1
+;     db  0, 1, 1, 0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 1, 0
+;     db  0, 1, 1, 0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 1, 0
+;     db  1, 0, 0, 1, 0, 1, 1, 0, 0, 1, 1, 0, 1, 0, 0, 1
