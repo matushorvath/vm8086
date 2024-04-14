56ece36aa558e6861647cfbf84d46d2a143abd04
Matus Horvath <matus.horvath@gmail.com>
2024-04-14 19:22:45 +0200

    Refactor SUB 16-bit tests

---
+++ test/sub_sbb_cmp_16/sub_sbb_cmp_16.asm
@@ -0,0 +1,21 @@
+%include "common.inc"
+
+
+section data_segment start=0x10000 nobits
+    resw 29
+test_ds_8:
+    resw 263
+test_ds_16:
+    resw 1
+
+
+section .text
+    dump_state
+
+    mov ax, 0x1000
+    mov ds, ax
+
+%include "sub_sbb_cmp_16_modes.inc"
+%include "sub_sbb_cmp_16_numbers.inc"
+
+     call power_off
