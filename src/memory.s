f8fcd1444dcf5f31ed84fdf5ff663ee42c275c68
Matus Horvath <matus.horvath@gmail.com>
2024-05-18 14:26:46 +0200

    Refactor read_seg_off_w, write_seg_off_w

--- src/memory.s
+++ src/memory.s
@@ -1,11 +1,9 @@
 .EXPORT calc_addr_b
+.EXPORT calc_addr_w
 .EXPORT calc_cs_ip_addr
 
 .EXPORT read_b
 .EXPORT write_b
-
-.EXPORT read_seg_off_w
-.EXPORT write_seg_off_w
 
 .EXPORT read_cs_ip_b
 .EXPORT read_cs_ip_w
@@ -67,6 +65,48 @@ calc_addr_b_done:
 .ENDFRAME
 
 ##########
+calc_addr_w:
+.FRAME seg, off; addr_lo, addr_hi, off_hi, tmp              # returns addr_lo, addr_hi
+    arb -4
+
+    # TODO optimize the common case when off != 0xffff and addr_lo != 0xfffff
+
+    # Calculate physical address of the lo byte
+    mul [rb + seg], 0x10, [rb + addr_lo]
+    add [rb + off], [rb + addr_lo], [rb + addr_lo]
+
+    # Wrap around address of lo byte to 20 bits
+    lt  [rb + addr_lo], 0x100000, [rb + tmp]
+    jnz [rb + tmp], calc_addr_b_addr_lo_done
+
+    add [rb + addr_lo], -0x100000, [rb + addr_lo]
+
+calc_addr_b_addr_lo_done:
+    # Increment offset with wrap around to 16 bits
+    add [rb + off], 1, [rb + off_hi]
+
+    lt  [rb + off_hi], 0x10000, [rb + tmp]
+    jnz [rb + tmp], calc_addr_b_off_hi_done
+
+    add [rb + off_hi], -0x10000, [rb + off_hi]
+
+calc_addr_b_off_hi_done:
+    # Calculate physical address of the hi byte
+    mul [rb + seg], 0x10, [rb + addr_hi]
+    add [rb + off_hi], [rb + addr_hi], [rb + addr_hi]
+
+    # Wrap around address of hi byte to 20 bits
+    lt  [rb + addr_hi], 0x100000, [rb + tmp]
+    jnz [rb + tmp], calc_addr_b_addr_hi_done
+
+    add [rb + addr_hi], -0x100000, [rb + addr_hi]
+
+calc_addr_b_addr_hi_done:
+    arb 4
+    ret 2
+.ENDFRAME
+
+##########
 calc_cs_ip_addr:
 .FRAME addr, tmp                                            # returns addr
     arb -2
@@ -91,59 +131,11 @@ calc_cs_ip_addr:
 
     add [rb + addr], -0x100000, [rb + addr]
 
+    # TODO this address should not be used to read word-sized values
+
 calc_cs_ip_addr_done:
     arb 2
     ret 0
-.ENDFRAME
-
-##########
-read_seg_off_w:
-.FRAME seg, off; value_lo, value_hi, addr                   # returns value_lo, value_hi
-    arb -3
-
-    add [rb + seg], 0, [rb - 1]
-    add [rb + off], 0, [rb - 2]
-    arb -2
-    call calc_addr_b
-    add [rb - 4], 0, [rb + addr]
-
-    add [rb + addr], 0, [rb - 1]
-    arb -1
-    call read_b
-    add [rb - 3], 0, [rb + value_lo]
-
-    add [rb + addr], 1, [rb - 1]                            # TODO wrap around to 20 bits
-    arb -1
-    call read_b
-    add [rb - 3], 0, [rb + value_hi]
-
-    arb 3
-    ret 2
-.ENDFRAME
-
-##########
-write_seg_off_w:
-.FRAME seg, off, value_lo, value_hi; addr
-    arb -1
-
-    add [rb + seg], 0, [rb - 1]
-    add [rb + off], 0, [rb - 2]
-    arb -2
-    call calc_addr_b
-    add [rb - 4], 0, [rb + addr]
-
-    add [rb + addr], 0, [rb - 1]
-    add [rb + value_lo], 0, [rb - 2]
-    arb -2
-    call write_b
-
-    add [rb + addr], 1, [rb - 1]                            # TODO wrap around to 20 bits
-    add [rb + value_hi], 0, [rb - 2]
-    arb -2
-    call write_b
-
-    arb 1
-    ret 4
 .ENDFRAME
 
 ##########
