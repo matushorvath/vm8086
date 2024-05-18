6ec2eba74f508188f3ae02f6da50717ef307337d
Matus Horvath <matus.horvath@gmail.com>
2024-05-18 17:19:14 +0200

    Configurable flag behavior; fix 9C pushf

--- test-cpu/config.s
+++ test-cpu/config.s
@@ -1,7 +1,12 @@
 .EXPORT config_enable_tracing
+.EXPORT config_flags_as_286
 
 # Tracing (0 - disable tracing, -1 - trace always, >0 - tracing past given address)
 config_enable_tracing:
+    db  0
+
+# Make flags behave like a real 8086/8088
+config_flags_as_286:
     db  0
 
 .EOF
