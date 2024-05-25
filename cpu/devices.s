d2cc0638768a91993d256a7cee4225a5bdd51e77
Matus Horvath <matus.horvath@gmail.com>
2024-05-25 15:18:24 +0200

    Split 20-8-12 WIP

--- cpu/devices.s
+++ cpu/devices.s
@@ -1,143 +1,153 @@
-#.EXPORT register_interrupt
-.EXPORT register_port
-#.EXPORT register_memory
-
-#.EXPORT handle_interrupt
+.EXPORT handle_interrupt
 .EXPORT handle_port_read
 .EXPORT handle_port_write
-#.EXPORT handle_memory_read
-#.EXPORT handle_memory_write
+.EXPORT handle_memory_read
+.EXPORT handle_memory_write
 
-# From error.s
-.IMPORT report_error
+# Data structures that need to be linked in:
+#
+# device_interrupts:
+#     pointer to a table of 256 records, each 1 byte
+#     device_interrupts = { callback }[256]
+#
+# device_ports:
+#     pointer to a two level table of 256 * 256 records, each 2 bytes
+#     device_ports = { device_ports_level_2 }[256]
+#     device_ports_level_2 = { read_callback, write_callback }[256]
+#
+# device_regions:
+#     pointer to a table of 256 records, each 2 bytes
+#     each region represents 4kB of memory
+#     device_regions = { read_callback, write_callback }[256]
 
-# TODO optimize data structures for speed, e.g. a hash table
-# TODO check for duplicate registration (same port/interrupt/overlapping region already registered)
+# From the config file
+.IMPORT device_interrupts
+.IMPORT device_ports
+.IMPORT device_regions
+
+# From util.s
+.IMPORT split_20_8_12
+
+# TODO functions to register devices dynamically
 
 ##########
-#interrupts:
-#    # interrupt, callback
-#.SYMBOL INTERRUPTS_SIZE 32 # = INTERRUPT_LENGTH * 16
-#.SYMBOL INTERRUPT_LENGTH 2
-#    ds  32, 0
-
-ports:
-    # port, read callback, write callback
-.SYMBOL PORTS_SIZE 768  # = PORT_LENGTH * 256
-.SYMBOL PORT_LENGTH 3
-    ds  768, 0
-
-regions:
-    # start address, stop address, read callback, write callback
-.SYMBOL REGIONS_SIZE 64 # = REGIONY_LENGTH * 16
-.SYMBOL REGIONY_LENGTH 4
-    ds  64, 0
-
-##########
-register_port:
-.FRAME port, read_callback, write_callback; rec_ptr, tmp
+handle_interrupt:
+.FRAME interrupt; callback, tmp
     arb -2
 
-    add ports, 0, [rb + rec_ptr]
+    # Is there any table at all?
+    jz  [device_interrupts], handle_interrupt_done
 
-register_port_loop:
-    add [rb + rec_ptr], 0, [ip + 1]
-    jz  [0], register_port_empty_found
+    # Is there a callback?
+    add [device_interrupts], [rb + interrupt], [ip + 1]
+    add [0], 0, [rb + callback]
 
-    add [rb + rec_ptr], PORT_LENGTH, [rb + rec_ptr]
+    jz  [rb + callback], handle_interrupt_done
 
-    lt  [rb + rec_ptr], PORTS_SIZE, [rb + tmp]
-    jnz [rb + tmp], register_port_loop
-
-    # No free space in the table
-    add register_port_no_space, 0, [rb - 1]
+    # Call the callback
+    add [rb + interrupt], 0, [rb - 1]
     arb -1
-    call report_error
+    call [rb + callback + 1]
 
-register_port_empty_found:
-    # Create a new record
-    add [rb + rec_ptr], 0, [ip + 3]
-    add [rb + port], 0, [0]
-
-    add [rb + rec_ptr], 1, [ip + 3]
-    add [rb + read_callback], 0, [0]
-
-    add [rb + rec_ptr], 2, [ip + 3]
-    add [rb + write_callback], 0, [0]
-
+handle_interrupt_done:
     arb 2
-    ret 3
-
-register_port_no_space:
-    db  "Cannot register port; no free space in the table", 0
+    ret 1
 .ENDFRAME
 
 ##########
 handle_port_read:
-.FRAME port; value, rec_ptr, tmp                            # returns value
-    arb -3
+.FRAME port; value, table, callback, tmp                    # returns value
+    arb -4
 
-    add ports, 0, [rb + rec_ptr]
+    # Is there any table at all?
+    jz  [device_ports], handle_port_read_done
 
-handle_port_read_loop:
-    add [rb + rec_ptr], 0, [ip + 1]
-    eq  [0], [rb + port], [rb + tmp]
-    jnz [rb + tmp], handle_port_read_found
+    # Is there a second level table?
+    add [device_ports], [rb + port], [ip + 1]
+    add [0], 0, [rb + table]
 
-    add [rb + rec_ptr], PORT_LENGTH, [rb + rec_ptr]
+    jz  [rb + table], handle_port_read_done
 
-    lt  [rb + rec_ptr], PORTS_SIZE, [rb + tmp]
-    jnz [rb + tmp], handle_port_read_loop
+    # Is there a read callback?
+    mul [rb + port], 2, [rb + tmp]
+    add [rb + table], [rb + tmp], [ip + 1]
+    add [0], 0, [rb + callback]
 
-    jz  0, handle_port_read_done
+    jz  [rb + callback], handle_port_read_done
 
-handle_port_read_found:
     # Call the read callback
-    add [rb + rec_ptr], 1, [ip + 1]
-    add [0], 0, [rb + tmp]
-
     add [rb + port], 0, [rb - 1]
     arb -1
-    call [rb + tmp + 1]
+    call [rb + callback + 1]
     add [rb - 3], 0, [rb + value]
 
 handle_port_read_done:
-    arb 3
+    arb 4
     ret 1
 .ENDFRAME
 
 ##########
 handle_port_write:
-.FRAME port, value; rec_ptr, tmp
-    arb -2
+.FRAME port, value; table, callback, tmp
+    arb -3
 
-    add ports, 0, [rb + rec_ptr]
+    # Is there any table at all?
+    jz  [device_ports], handle_port_write_done
 
-handle_port_write_loop:
-    add [rb + rec_ptr], 0, [ip + 1]
-    eq  [0], [rb + port], [rb + tmp]
-    jnz [rb + tmp], handle_port_write_found
+    # Is there a second level table?
+    add [device_ports], [rb + port], [ip + 1]
+    add [0], 0, [rb + table]
 
-    add [rb + rec_ptr], PORT_LENGTH, [rb + rec_ptr]
+    jz  [rb + table], handle_port_write_done
 
-    lt  [rb + rec_ptr], PORTS_SIZE, [rb + tmp]
-    jnz [rb + tmp], handle_port_write_loop
+    # Is there a write callback?
+    mul [rb + port], 2, [rb + tmp]
+    add [rb + table], [rb + tmp], [rb + tmp]
+    add 1, [rb + tmp], [ip + 1]
+    add [0], 0, [rb + callback]
 
-    jz  0, handle_port_write_done
+    jz  [rb + callback], handle_port_write_done
 
-handle_port_write_found:
     # Call the write callback
-    add [rb + rec_ptr], 2, [ip + 1]
-    add [0], 0, [rb + tmp]
-
     add [rb + port], 0, [rb - 1]
     add [rb + value], 0, [rb - 2]
     arb -2
-    call [rb + tmp + 2]
+    call [rb + callback + 2]
 
 handle_port_write_done:
-    arb 2
+    arb 3
     ret 2
 .ENDFRAME
+
+##########
+handle_memory_read:
+.FRAME address; value, callback, tmp                       # returns value
+    arb -X
+
+    # Is there any table at all?
+    jz  [device_regions], handle_memory_read_done
+
+    # Split the address into an 8-bit part and a 12-bit part
+    add [rb + address], 0, [rb - 1]
+    arb -1
+    call split_20_8_12      # [rb - 3] and [rb - 4] are used below
+
+    # Is there a callback?
+    mul [rb - 3], 2, [rb + tmp]
+    add [device_regions], [rb + tmp], [ip + 1]
+    add [0], 0, [rb + callback]
+
+    jz  [rb + callback], handle_memory_read_done
+
+    # Call the callback
+    add [rb + interrupt], 0, [rb - 1]
+    arb -1
+    call [rb + callback + 1]
+
+handle_memory_read_done:
+    arb X
+    ret 1
+.ENDFRAME
+
 
 .EOF
--- cpu/util.s
+++ cpu/util.s
@@ -1,5 +1,6 @@
 .EXPORT check_range
 .EXPORT split_16_8_8
+.EXPORT split_20_8_12
 
 # From error.s
 .IMPORT report_error
@@ -66,6 +67,66 @@ split_16_8_8_pow_lo:
     db  0x00000001, 0x00000002, 0x00000004, 0x00000008, 0x00000010, 0x00000020, 0x00000040, 0x00000080
 split_16_8_8_pow_hi:
     db  0x00000100, 0x00000200, 0x00000400, 0x00000800, 0x00001000, 0x00002000, 0x00004000, 0x00008000
+.ENDFRAME
+
+##########
+split_20_8_12:
+.FRAME value; value_lo_12, value_hi_8, tmp                  # returns value_lo_12, value_hi_8
+    arb -3
+
+    add 0, 0, [rb + value_hi_8]
+    add [rb + value], 0, [rb + value_lo_12]
+
+    lt  0x7ffff, [rb + value], [rb + tmp]
+    jz  [rb + tmp], split_20_8_14_bit_19
+    add [rb + value_lo_12], -0x80000, [rb + value_lo_12]
+    add [rb + value_hi_8], 0x80
+
+split_20_8_14_bit_19:
+    lt  0x3ffff, [rb + value], [rb + tmp]
+    jz  [rb + tmp], split_20_8_14_bit_18
+    add [rb + value_lo_12], -0x40000, [rb + value_lo_12]
+    add [rb + value_hi_8], 0x40
+
+split_20_8_14_bit_18:
+    lt  0x1ffff, [rb + value], [rb + tmp]
+    jz  [rb + tmp], split_20_8_14_bit_17
+    add [rb + value_lo_12], -0x20000, [rb + value_lo_12]
+    add [rb + value_hi_8], 0x20
+
+split_20_8_14_bit_17:
+    lt  0x0ffff, [rb + value], [rb + tmp]
+    jz  [rb + tmp], split_20_8_14_bit_16
+    add [rb + value_lo_12], -0x10000, [rb + value_lo_12]
+    add [rb + value_hi_8], 0x10
+
+split_20_8_14_bit_16:
+    lt  0x07fff, [rb + value], [rb + tmp]
+    jz  [rb + tmp], split_20_8_14_bit_15
+    add [rb + value_lo_12], -0x08000, [rb + value_lo_12]
+    add [rb + value_hi_8], 0x08
+
+split_20_8_14_bit_15:
+    lt  0x03fff, [rb + value], [rb + tmp]
+    jz  [rb + tmp], split_20_8_14_bit_14
+    add [rb + value_lo_12], -0x04000, [rb + value_lo_12]
+    add [rb + value_hi_8], 0x04
+
+split_20_8_14_bit_14:
+    lt  0x01fff, [rb + value], [rb + tmp]
+    jz  [rb + tmp], split_20_8_14_bit_13
+    add [rb + value_lo_12], -0x02000, [rb + value_lo_12]
+    add [rb + value_hi_8], 0x02
+
+split_20_8_14_bit_13:
+    lt  0x00fff, [rb + value], [rb + tmp]
+    jz  [rb + tmp], split_20_8_14_bit_12
+    add [rb + value_lo_12], -0x01000, [rb + value_lo_12]
+    add [rb + value_hi_8], 0x01
+
+split_20_8_14_bit_12:
+    arb 3
+    ret 1
 .ENDFRAME
 
 .EOF
