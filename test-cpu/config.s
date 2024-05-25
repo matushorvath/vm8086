44ffde8833d463ea553424716c9ba287df6daf3a
Matus Horvath <matus.horvath@gmail.com>
2024-05-25 18:49:26 +0200

    Fix test-cpu after new I/O framework

--- test-cpu/config.s
+++ test-cpu/config.s
@@ -4,6 +4,10 @@
 .EXPORT config_bcd_as_bochs
 .EXPORT config_de_fault_as_bochs
 
+.EXPORT device_interrupts
+.EXPORT device_ports
+.EXPORT device_regions
+
 # Tracing (0 - disable tracing, -1 - trace always, >0 - tracing past given address)
 config_enable_tracing:
     db  0
@@ -22,6 +26,12 @@ config_bcd_as_bochs:
 
 # Make #DE push address after the failing DIV/IDIV
 config_de_fault_as_bochs: # TODO rename config_de_fault_as_286
+    db  0
+
+# No devices
+device_interrupts:
+device_ports:
+device_regions:
     db  0
 
 .EOF
