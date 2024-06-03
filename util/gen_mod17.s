c7668a73fa901fe0b969947081833a12a6735473
Matus Horvath <matus.horvath@gmail.com>
2024-06-03 10:39:17 +0200

    Refactor mod* tables, add mod5 for CGA

--- util/gen_mod17.s
+++ util/gen_mod17.s
@@ -3,90 +3,25 @@
 .IMPORT print_num
 .IMPORT print_str
 
+# We need 8 bits = 256, nearest number is 272, 272/17 = 16
+.SYMBOL LIMIT 16
+
     arb stack
 
     add header, 0, [rb - 1]
     arb -1
     call print_str
 
-b7_loop:
-    b6_loop:
-        b5_loop:
-            b4_loop:
-                # Print line start
-                add line_start, 0, [rb - 1]
-                arb -1
-                call print_str
+    add 0, 0, [number]
 
-                add 1, 0, [new_line]
+mod_loop:
+    add mod_line, 0, [rb - 1]
+    arb -1
+    call print_str
 
-                b3_loop:
-                    b2_loop:
-                        b1_loop:
-                            b0_loop:
-                                # Skip separator before the first number
-                                jnz [new_line], skip_separator
-
-                                out ','
-                                out ' '
-
-                            skip_separator:
-                                add 0, 0, [new_line]
-
-                                # Print current number mod 17
-                                add [curr_mod_17], 0, [rb - 1]
-                                arb -1
-                                call print_num
-
-                                # Increase current number mod 17
-                                add [curr_mod_17], 1, [curr_mod_17]
-                                lt  [curr_mod_17], 17, [tmp]
-                                jnz [tmp], after_increment
-                                add [curr_mod_17], -17, [curr_mod_17]
-
-                            after_increment:
-                                add [b0], 1, [b0]
-                                eq  [b0], 2, [tmp]
-                                jz  [tmp], b0_loop
-                                add 0, 0, [b0]
-
-                            add [b1], 1, [b1]
-                            eq  [b1], 2, [tmp]
-                            jz  [tmp], b1_loop
-                            add 0, 0, [b1]
-
-                        add [b2], 1, [b2]
-                        eq  [b2], 2, [tmp]
-                        jz  [tmp], b2_loop
-                        add 0, 0, [b2]
-
-                    add [b3], 1, [b3]
-                    eq  [b3], 2, [tmp]
-                    jz  [tmp], b3_loop
-                    add 0, 0, [b3]
-
-                # Print line end
-                out 10
-
-                add [b4], 1, [b4]
-                eq  [b4], 2, [tmp]
-                jz  [tmp], b4_loop
-                add 0, 0, [b4]
-
-            add [b5], 1, [b5]
-            eq  [b5], 2, [tmp]
-            jz  [tmp], b5_loop
-            add 0, 0, [b5]
-
-        add [b6], 1, [b6]
-        eq  [b6], 2, [tmp]
-        jz  [tmp], b6_loop
-        add 0, 0, [b6]
-
-    add [b7], 1, [b7]
-    eq  [b7], 2, [tmp]
-    jz  [tmp], b7_loop
-    add 0, 0, [b7]
+    add [number], 1, [number]
+    eq  [number], LIMIT + 1, [tmp]
+    jz  [tmp], mod_loop
 
     add footer, 0, [rb - 1]
     arb -1
@@ -94,33 +29,17 @@ b7_loop:
 
     hlt
 
-new_line:
-    db  1
-b0:
+number:
     db  0
-b1:
-    db  0
-b2:
-    db  0
-b3:
-    db  0
-b4:
-    db  0
-b5:
-    db  0
-b6:
-    db  0
-b7:
-    db  0
-curr_mod_17:
+index:
     db  0
 tmp:
     db  0
 
 header:
     db  ".EXPORT mod17", 10, 10, "# Generated using gen_mod17.s", 10, 10, "mod17:", 10, 0
-line_start:
-    db  "    db  ", 0
+mod_line:
+    db  "    db  0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10,", " 11, 12, 13, 14, 15, 16", 10, 0
 footer:
     db  10, ".EOF", 10, 0
 
---
+++ util/gen_mod5.s
@@ -0,0 +1,86 @@
+# Utility to generate mod9.s
+
+.IMPORT print_str
+.IMPORT print_num
+
+# We need 10 bits = 1024, nearest number is 1025, 1025/5 = 205
+.SYMBOL LIMIT 205
+
+    arb stack
+
+    add header, 0, [rb - 1]
+    arb -1
+    call print_str
+
+    add 0, 0, [number]
+
+mod_loop:
+    add mod_line, 0, [rb - 1]
+    arb -1
+    call print_str
+
+    add [number], 1, [number]
+    eq  [number], LIMIT + 1, [tmp]
+    jz  [tmp], mod_loop
+
+    add div_header, 0, [rb - 1]
+    arb -1
+    call print_str
+
+    add 0, 0, [number]
+
+div_loop:
+    add div_line_start, 0, [rb - 1]
+    arb -1
+    call print_str
+
+    add 5, 0, [index]
+
+div_inside_loop:
+    add [number], 0, [rb - 1]
+    arb -1
+    call print_num
+
+    add [index], -1, [index]
+    jz  [index], div_inside_done
+
+    out ','
+    out ' '
+
+    jz  0, div_inside_loop
+
+div_inside_done:
+    out 10
+
+    add [number], 1, [number]
+    eq  [number], LIMIT + 1, [tmp]
+    jz  [tmp], div_loop
+
+    add footer, 0, [rb - 1]
+    arb -1
+    call print_str
+
+    hlt
+
+number:
+    db  0
+index:
+    db  0
+tmp:
+    db  0
+
+header:
+    db  ".EXPORT div5", 10, ".EXPORT mod5", 10, 10, "# Generated using gen_mod5.s", 10, 10, "div5:", 10, 0
+mod_line:
+    db  "    db  0, 1, 2, 3, 4", 10, 0
+div_header:
+    db  10, "mod5:", 10, 0
+div_line_start:
+    db  "    db  ", 0
+footer:
+    db  10, ".EOF", 10, 0
+
+    ds  100, 0
+stack:
+
+.EOF
