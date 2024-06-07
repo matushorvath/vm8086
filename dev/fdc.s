c3de3f67ab470a49ea91a128419bc102a0039234
Matus Horvath <matus.horvath@gmail.com>
2024-06-07 11:37:44 +0200

    Analyze FDC commands

--- dev/fdc.s
+++ dev/fdc.s
@@ -104,11 +104,88 @@ fdc_status_read:
 ##########
 fdc_data_write:
 .FRAME addr, value;
-    # TODO
-
     out 'D' # TODO remove
     out 'w'
     out ' '
+
+    # Is the FDC in the middle of processing a command?
+    jz  [fdc_command_state], fdc_data_write_idle
+
+    # Yes, is this the command phase?
+    jnz [fdc_result_phase], fdc_data_write_invalid
+
+    # Yes, use the state as a label to jump to
+    jz  0, [fdc_command_state]
+
+fdc_data_write_idle:
+    # Accept a new command
+
+    # MT MF SK 0 0 1 1 0: cmd = read_data; save MT MF SK; -> _hd_us
+    # MT MF SK 0 1 1 0 0: cmd = read_deleted_data; save MT MF SK; -> _hd_us
+    # MT MF  0 0 0 1 0 1: cmd = write_data; save MT MF; -> _hd_us
+    # MT MF  0 0 1 0 0 1: cmd = write_deleted_data; save MT MF; -> _hd_us
+    #  0 MF SK 0 0 0 1 0: cmd = read_track; save MF SK; -> _hd_us
+    #  0 MF  0 0 1 0 1 0: cmd = read_id; save MF; -> _hd_us
+    #  0 MF  0 0 1 1 0 0: cmd = format_track; save MF; -> _hd_us
+    # MT MF SK 1 0 0 0 1: cmd = scan_equal; save MT MF SK; -> _hd_us
+    # MT MF SK 1 1 0 0 1: cmd = scan_low_or_equal; save MT MF SK; -> _hd_us
+    # MT MF SK 1 1 1 0 1: cmd = scan_high_or_equal; save MT MF SK; -> _hd_us
+    #  0  0  0 0 0 1 1 1: cmd = recalibrate; -> _hd_us
+    #  0  0  0 0 1 0 0 0: cmd = sense_interrupt_status; -> execute, _st0 (or separate satus for SIS)
+    #  0  0  0 0 0 0 1 1: cmd = specify; -> _specify_srt_hut
+    #  0  0  0 0 0 0 1 1: cmd = sense_driver_status; -> _hd_us
+    #  0  0  0 0 1 1 1 1: cmd = seek; -> _hd_us
+    # other             : cmd = invalid; -> _hd_us
+
+_hd_us
+    # X X X X X HD US1 US0: save head (HD), save drive (US0);
+        -> _c (default); execute,
+        execute, -> _st0 (cmd=read_id);
+        -> _n (cmd=format_track);
+        execute, -> idle (cmd=recalibrate)
+        execute, -> _st3 (cmd=sense_driver_status)
+        execute, -> _ncn (cmd=seek)
+        execute, -> _st0 (cmd=invalid)
+
+_c
+    # C: save cylinder (C); -> _h
+
+_h
+    # H: verify that H === HD (from first byte); -> _r
+
+_r
+    # R: save sector (record, R); -> _n
+
+_n
+    # N: save number of bytes in sector (?) perhaps size to read/write?; -> _eot (default); _sc (cmd=format_track, or separate state for format?)
+
+_eot
+    # EOT: end of track, final sector number on a cylinder (does it need saving?); -> _gpl
+
+_gpl
+    # GPL: gap 3 length (spacing between sectors) perhaps just verify it is correct?; -> _dtl (default); _d (cmd=format_track or separate state for format)
+
+_dtl
+    # DTL data length; if N is 0, DTL is the length to read/write to a sector (save/verify with N); result phase = 1, execute, -> _st0
+
+_sc
+    # SC: number of sectors per cylinder; -> _gpl (or separate state for format?)
+
+_d
+    # D: data pattern to be written to a sector; execute, -> _st0
+
+_specify_srt_hut
+    # SRT 4b, HUT 4b: read -> _specify_hlt_nd
+
+_specify_hlt_nd
+    # HLT 7b ND 1b: read -> idle
+
+_ncn
+    # NCN: -> idle
+
+fdc_data_write_invalid:
+    # in: X X X X X HD US1 US0
+    # out ST0
 
     ret 2
 .ENDFRAME
@@ -124,6 +201,33 @@ fdc_data_read:
     out 'r'
     out ' '
 
+_st0
+    # read ST0 -> _st1 (default); idle (cmd=invalid); _pcn (cmd=sense_interrupt_status)
+
+_st1
+    # read ST1 -> _c (cmd=read_data, read_deleted_data); -> _st2 (default)
+
+_st2
+    # read ST2 -> _c
+
+_c
+    # read C -> _h
+
+_h
+    # read H -> _r
+
+_r
+    # read R (sector) -> _n
+
+_n
+    # read N (number of bytes) -> idle
+
+_pcn
+    # read PCN (present cylinder number) position of the head -> idle
+
+_st3
+    # read ST3 -> idle
+
     arb 1
     ret 1
 .ENDFRAME
@@ -131,7 +235,8 @@ fdc_data_read:
 ##########
 fdc_control_write:
 .FRAME addr, value;
-    # TODO
+    # TODO this is I think used to detect floppy type
+    # if yes, we need to return errors (when reading?) unless the speed is set correctly
 
     out 'C' # TODO remove
     out ' '
@@ -186,6 +291,9 @@ fdc_dor_enable_motor_b:
     db  0
 
 fdc_result_phase:
+    db  0
+
+fdc_command_state:
     db  0
 
 .EOF
