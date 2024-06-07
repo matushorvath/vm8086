62b84b85f9b312af02300bf89cb7c6fb2e3a2dd2
Matus Horvath <matus.horvath@gmail.com>
2024-06-07 16:04:49 +0200

    Initial fdc state machine

--- dev/fdc.s
+++ dev/fdc.s
@@ -117,27 +117,27 @@ fdc_data_write:
     jz  [fdc_cmd_state], fdc_data_write_idle
 
     # Yes, is this the command phase?
-    # TODO how is write in result phase handled correctly?
     jnz [fdc_cmd_result_phase], fdc_data_write_invalid
 
     # Yes, use the state as a label to jump to
     jz  0, [fdc_cmd_state]
 
 fdc_data_write_idle:
-    # Parse the first byte of a command
+    # Parse the first byte of a new command
+    # MT MF SK CMD_CODE(5)
     mul [rb + value], 8, [rb + value_x8]
 
     # Save MT, MF, SK
     add value_bits + 7, [rb + value_x8], [ip + 1]
-    add [0], 0, [fdc_cmd_mt] # TODO execute multitrack operation
+    add [0], 0, [fdc_cmd_multi_track] # TODO execute multitrack operation
 
     add value_bits + 6, [rb + value_x8], [ip + 1]
-    add [0], 0, [fdc_cmd_mf] # TODO validate that FM/MFM encoding matches the disk
+    add [0], 0, [fdc_cmd_mfm] # TODO validate that FM/MFM encoding matches the disk
 
     add value_bits + 5, [rb + value_x8], [ip + 1]
-    add [0], 0, [fdc_cmd_sk] # TODO execute support for skip deleted data
+    add [0], 0, [fdc_cmd_skip_deleted] # TODO execute support for skip deleted data
 
-    # Read bottom 5 bits as the command
+    # Read bottom 5 bits as the command code
     add value_bits + 4, [rb + value_x8], [ip + 1]
     mul [0], 0b00010000, [fdc_cmd_code]
 
@@ -145,43 +145,159 @@ fdc_data_write_idle:
     add nibbles, [rb + tmp], [ip + 1]
     add [0], [fdc_cmd_code], [fdc_cmd_code]
 
-    # We are now in command phase, the command wasn't executed yet
+    # We are now in command phase
     add 0, 0, [fdc_cmd_result_phase]
-    add 0, 0, [fdc_cmd_executed]
 
     # Handle the state transition
     add fdc_data_write_idle_table, [fdc_cmd_code], [ip + 2]
     jz  0, [0]
 
 fdc_data_write_idle_table:
-    db  fdc_data_write_idle_to_hd_us
+    db  fdc_data_write_invalid                              #          00000
+    db  fdc_data_write_invalid                              #          00001
+    db  fdc_data_write_idle_to_hd_us                        #  0 MF SK 00010: read_track
+    db  fdc_data_write_idle_to_srt_hut                      #  0  0  0 00011: specify
+    db  fdc_data_write_idle_to_hd_us                        #  0  0  0 00100: sense_drive_status
+    db  fdc_data_write_idle_to_hd_us                        # MT MF  0 00101: write_data
+    db  fdc_data_write_idle_to_hd_us                        # MT MF SK 00110: read_data
+    db  fdc_data_write_idle_to_hd_us                        #  0  0  0 00111: recalibrate
+    db  fdc_data_write_exec_sense_interrupt_status          #  0  0  0 01000: sense_interrupt_status
+    db  fdc_data_write_idle_to_hd_us                        # MT MF  0 01001: write_deleted_data
+    db  fdc_data_write_idle_to_hd_us                        #  0 MF  0 01010: read_id
+    db  fdc_data_write_invalid                              #          01011
+    db  fdc_data_write_idle_to_hd_us                        # MT MF SK 01100: read_deleted_data
+    db  fdc_data_write_idle_to_hd_us                        #  0 MF  0 01101: format_track
+    db  fdc_data_write_invalid                              #          01110
+    db  fdc_data_write_idle_to_hd_us                        #  0  0  0 01111: seek
+    db  fdc_data_write_invalid                              #          10000
+    db  fdc_data_write_idle_to_hd_us                        # MT MF SK 10001: scan_equal
+    db  fdc_data_write_invalid                              #          10010
+    db  fdc_data_write_invalid                              #          10011
+    db  fdc_data_write_invalid                              #          10100
+    db  fdc_data_write_invalid                              #          10101
+    db  fdc_data_write_invalid                              #          10110
+    db  fdc_data_write_invalid                              #          10111
+    db  fdc_data_write_invalid                              #          11000
+    db  fdc_data_write_idle_to_hd_us                        # MT MF SK 11001: scan_low_or_equal
+    db  fdc_data_write_invalid                              #          11010
+    db  fdc_data_write_invalid                              #          11011
+    db  fdc_data_write_invalid                              #          11100
+    db  fdc_data_write_idle_to_hd_us                        # MT MF SK 11101: scan_high_or_equal
+    db  fdc_data_write_invalid                              #          11110
+    db  fdc_data_write_invalid                              #          11111
 
-    # MT MF SK 0 0 1 1 0: cmd_code = read_data; save MT MF SK; -> _hd_us
-    # MT MF SK 0 1 1 0 0: cmd_code = read_deleted_data; save MT MF SK; -> _hd_us
-    # MT MF  0 0 0 1 0 1: cmd_code = write_data; save MT MF; -> _hd_us
-    # MT MF  0 0 1 0 0 1: cmd_code = write_deleted_data; save MT MF; -> _hd_us
-    #  0 MF SK 0 0 0 1 0: cmd_code = read_track; save MF SK; -> _hd_us
-    #  0 MF  0 0 1 0 1 0: cmd_code = read_id; save MF; -> _hd_us
-    #  0 MF  0 0 1 1 0 0: cmd_code = format_track; save MF; -> _hd_us
-    # MT MF SK 1 0 0 0 1: cmd_code = scan_equal; save MT MF SK; -> _hd_us
-    # MT MF SK 1 1 0 0 1: cmd_code = scan_low_or_equal; save MT MF SK; -> _hd_us
-    # MT MF SK 1 1 1 0 1: cmd_code = scan_high_or_equal; save MT MF SK; -> _hd_us
-    #  0  0  0 0 0 1 1 1: cmd_code = recalibrate; -> _hd_us
-    #  0  0  0 0 1 0 0 0: cmd_code = sense_interrupt_status; -> execute, _st0 (or separate status for SIS)
-    #  0  0  0 0 0 0 1 1: cmd_code = specify; -> _specify_srt_hut
-    #  0  0  0 0 0 0 1 1: cmd_code = sense_driver_status; -> _hd_us
-    #  0  0  0 0 1 1 1 1: cmd_code = seek; -> _hd_us
-    # other             : cmd_code = invalid; -> _st0 and next idle state
+fdc_data_write_idle_to_hd_us:
+    # Next state is write HD US
+    add fdc_data_write_hd_us, 0, [fdc_cmd_state]
+    jz  0, fdc_data_write_done
 
-_hd_us
-    # X X X X X HD US1 US0: save head (HD), save drive (US0);
-        -> _c (default); execute,
-        execute, -> _st0 (cmd=read_id);
-        -> _n (cmd=format_track);
-        execute, -> idle (cmd=recalibrate)
-        execute, -> _st3 (cmd=sense_driver_status)
-        execute, -> _ncn (cmd=seek)
-        execute, -> _st0 (cmd=invalid)
+fdc_data_write_idle_to_srt_hut:
+    # Next state is write SRT HUT
+    add fdc_data_write_srt_hut, 0, [fdc_cmd_state]
+    jz  0, fdc_data_write_done
+
+fdc_data_write_exec_sense_interrupt_status:
+    # Execute sense interrupt status, next state is read ST0
+    # Bits 5, 6, 7 have already been set up by previous seek or recalibrate command
+    add 1, 0, [fdc_cmd_result_phase]
+    add fdc_data_read_st0, 0, [fdc_cmd_state]
+    jz  0, fdc_data_write_done
+
+fdc_data_write_hd_us:
+    # Parse head and unit select information
+    # X X X X X HD US(2)
+
+    mul [rb + value], 8, [rb + value_x8]
+
+    # Save HD and US
+    add value_bits + 2, [rb + value_x8], [ip + 1]
+    add [0], 0, [fdc_cmd_head]
+
+    add value_bits + 1, [rb + value_x8], [ip + 1]
+    mul [0], 0x00000010, [fdc_cmd_unit_select]
+    add value_bits + 0, [rb + value_x8], [ip + 1]
+    add [0], [fdc_cmd_unit_select], [fdc_cmd_unit_select]
+
+    # Handle the state transition
+    add fdc_data_write_hd_us_table, [fdc_cmd_code], [ip + 2]
+    jz  0, [0]
+
+fdc_data_write_hd_us_table:
+    db  fdc_data_write_invalid                              # 00000
+    db  fdc_data_write_invalid                              # 00001
+    db  fdc_data_write_hd_us_to_c                           # 00010: read_track
+    db  fdc_data_write_invalid                              # 00011: specify
+    db  fdc_data_write_exec_sense_drive_status              # 00100: sense_drive_status
+    db  fdc_data_write_hd_us_to_c                           # 00101: write_data
+    db  fdc_data_write_hd_us_to_c                           # 00110: read_data
+    db  fdc_data_write_exec_recalibrate                     # 00111: recalibrate
+    db  fdc_data_write_invalid                              # 01000: sense_interrupt_status
+    db  fdc_data_write_hd_us_to_c                           # 01001: write_deleted_data
+    db  fdc_data_write_exec_read_id                         # 01010: read_id
+    db  fdc_data_write_invalid                              # 01011
+    db  fdc_data_write_hd_us_to_c                           # 01100: read_deleted_data
+    db  fdc_data_write_hd_us_to_n                           # 01101: format_track
+    db  fdc_data_write_invalid                              # 01110
+    db  fdc_data_write_hd_us_to_ncn                         # 01111: seek
+    db  fdc_data_write_invalid                              # 10000
+    db  fdc_data_write_hd_us_to_c                           # 10001: scan_equal
+    db  fdc_data_write_invalid                              # 10010
+    db  fdc_data_write_invalid                              # 10011
+    db  fdc_data_write_invalid                              # 10100
+    db  fdc_data_write_invalid                              # 10101
+    db  fdc_data_write_invalid                              # 10110
+    db  fdc_data_write_invalid                              # 10111
+    db  fdc_data_write_invalid                              # 11000
+    db  fdc_data_write_hd_us_to_c                           # 11001: scan_low_or_equal
+    db  fdc_data_write_invalid                              # 11010
+    db  fdc_data_write_invalid                              # 11011
+    db  fdc_data_write_invalid                              # 11100
+    db  fdc_data_write_hd_us_to_c                           # 11101: scan_high_or_equal
+    db  fdc_data_write_invalid                              # 11110
+    db  fdc_data_write_invalid                              # 11111
+
+fdc_data_write_hd_us_to_c:
+    # Next state is write C
+    add fdc_data_write_c, 0, [fdc_cmd_state]
+    jz  0, fdc_data_write_done
+
+fdc_data_write_exec_read_id:
+    # Execute read id
+    # TODO
+
+    # Next state is read ST0
+    add 1, 0, [fdc_cmd_result_phase]
+    add fdc_data_read_st0, 0, [fdc_cmd_state]
+    jz  0, fdc_data_write_done
+
+fdc_data_write_hd_us_to_n:
+    # Next state is write N
+    add fdc_data_write_n, 0, [fdc_cmd_state]
+    jz  0, fdc_data_write_done
+
+fdc_data_write_exec_recalibrate:
+    # Execute recalibrate
+    # TODO
+
+    # Next state is idle
+    add 0, 0, [fdc_cmd_state]
+    jz  0, fdc_data_write_done
+
+fdc_data_write_exec_sense_drive_status:
+    # Execute sense drive status
+    # TODO
+
+    # Next state is read ST3
+    add 1, 0, [fdc_cmd_result_phase]
+    add fdc_data_read_st3, 0, [fdc_cmd_state]
+    jz  0, fdc_data_write_done
+
+fdc_data_write_hd_us_to_ncn:
+    # Next state is write NCN
+    add fdc_data_write_ncn, 0, [fdc_cmd_state]
+    jz  0, fdc_data_write_done
+
+
 
 _c
     # C: save cylinder (C); -> _h
@@ -220,13 +336,19 @@ _ncn
     # NCN: -> idle
 
 fdc_data_write_invalid:
-    # in: X X X X X HD US1 US0
-    # out ST0
+    # Whatever was received is discarded, return status of 0x80
+    add 0x80, 0, [fdc_cmd_st0]
 
-    # TODO after the execution phase, interrupt will occur
+    # Current command code is invalid, next state is read ST0
+    add 0, 0, [fdc_cmd_code]
+    add 1, 0, [fdc_cmd_result_phase]
+    add fdc_data_read_st0, 0, [fdc_cmd_state]
 
+fdc_data_write_done:
     ret 2
 .ENDFRAME
+
+    # TODO after the execution phase, interrupt will occur
 
 ##########
 fdc_data_read:
