33257f2fef7ab8eb1724dbb7121643084a967412
Matus Horvath <matus.horvath@gmail.com>
2024-07-07 20:00:15 +0200

    Move text mode initialization to text_mode.s

--- cga/screen.s
+++ cga/screen.s
@@ -2,9 +2,6 @@
 .EXPORT enable_disable_screen
 
 .EXPORT screen_needs_redraw
-.EXPORT screen_page_size
-.EXPORT screen_text_negative_row_size
-.EXPORT screen_text_row_shr_table
 .EXPORT screen_width_chars
 .EXPORT screen_height_chars
 
@@ -12,23 +9,16 @@
 .IMPORT initialize_graphics_mode
 .IMPORT redraw_screen_graphics
 
-# From text_palette.s
-.IMPORT reinitialize_text_palette
-
 # From text_mode.s
+.IMPORT initialize_text_mode
 .IMPORT redraw_screen_text
 
 # From registers.s
-.IMPORT mode_high_res_text
 .IMPORT mode_graphics
 .IMPORT mode_enable_output
 
 # From status_bar.s
 .IMPORT redraw_status_bar
-
-# From util/shr.s
-.IMPORT shr_0
-.IMPORT shr_1
 
 ##########
 reset_screen:
@@ -58,24 +48,12 @@ reset_screen:
 reset_screen_text:
     # Text mode
 
-    # Negative value of screen row size; -160 for 80x25, -80 for 40x25
-    mul [mode_high_res_text], -80, [screen_text_negative_row_size]
-    add [screen_text_negative_row_size], -80, [screen_text_negative_row_size]
-
-    # Right shift table used to divide address into rows, shr_1 for 80x25, shr_0 for 40x25
-    add reset_screen_row_shr_tables, [mode_high_res_text], [ip + 1]
-    add [0], 0, [screen_text_row_shr_table]
-
     # Screen width is 80 chars (even in 40 char mode), height is 25 chars
     add 80, 0, [screen_width_chars]
     add 25, 0, [screen_height_chars]
 
-    # Page size is 25 rows * 80/160 bytes per row = 2000/4000 bytes depending on column count
-    add [mode_high_res_text], 1, [screen_page_size]
-    mul [screen_page_size], 2000, [screen_page_size]
-
-    # Initialize the palette for text mode
-    call reinitialize_text_palette
+    # Prepare for drawing the text mode
+    call initialize_text_mode
 
     # Redraw the screen
     call redraw_screen_text
@@ -85,10 +63,6 @@ reset_screen_redraw_status_bar:
     call redraw_status_bar
 
     ret 0
-
-reset_screen_row_shr_tables:
-    db  shr_0
-    db  shr_1
 .ENDFRAME
 
 ##########
@@ -123,20 +97,6 @@ enable_screen_done:
 # Flag to mark that the screen will need a redraw once output is enabled again
 screen_needs_redraw:
     db  0
-
-# One screen page size
-# TODO move to text_mode
-screen_page_size:
-    db  4000                            # 25 rows * 160 bytes per row
-
-# Negative value of screen row size; -160 for 80x25, -80 for 40x25
-# TODO move to text_mode
-screen_text_negative_row_size:
-    db  -160
-# Right shift table used to divide address into rows, shr_1 for 80x25, shr_0 for 40x25
-# TODO move to text_mode
-screen_text_row_shr_table:
-    db  shr_1
 
 # Screen width and height, for placing the status bar
 screen_width_chars:
