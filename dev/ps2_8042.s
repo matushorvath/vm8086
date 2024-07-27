3910a00d37489119b0fe719e486b6eb0056ada74
Matus Horvath <matus.horvath@gmail.com>
2024-07-27 15:15:52 +0200

    Reboot works, shuts down the VM

--- dev/ps2_8042.s
+++ dev/ps2_8042.s
@@ -6,12 +6,15 @@
 # From cpu/execute.s
 .IMPORT halt
 
+# From libxib.a
+.IMPORT print_str
+
 # The 8042 PS/2 Controller wasn't included in the IBM PC/XT
 # It's only supported partially, as an API to reset the CPU, which will cleanly shut down the VM
 
 ##########
 ps2_ports:
-    db  0x64, 0x00, ps2_command_write                       # Command register
+    db  0x64, 0x00, 0, ps2_command_write                    # Command register
 
     db  -1, -1, -1, -1
 
@@ -35,12 +38,19 @@ ps2_command_write:
     eq  [rb + value], 0xfe, [rb + tmp]
     jz  [rb + tmp], .done
 
+    add .reboot_msg, 0, [rb - 1]
+    arb -1
+    call print_str
+
     # Instead of just resetting, shut down the VM
     add 1, 0, [halt]
 
 .done:
     arb 1
     ret 2
+
+.reboot_msg:
+    db  10, "vm8086: software triggered reboot, shutting down", 10, 10, 0
 .ENDFRAME
 
 .EOF
