00c2d30b07c6cce6d8d10fa20b4a9c5d95d7ea23
Matus Horvath <matus.horvath@gmail.com>
2024-09-02 12:17:05 +0200

    Animated sixels

--- docs/sixels/index.js
+++ docs/sixels/index.js
@@ -1,13 +1,19 @@
+import timers from 'node:timers/promises';
+
 const main = async () => {
-    // TODO p2 = 1: keep 0 bits unchanged
-    process.stdout.write("\x1bPq"); // enter sixel mode
+    for (let j = 0; j < 1000; j++) {
+        process.stdout.write("\x1bPq"); // enter sixel mode
 
-    for (let i = 0; i < 500; i++) {
-        const pos = 6 - Math.round((Math.sin(2 * Math.PI * i / 500) + 1) * 6 / 2);
-        process.stdout.write(String.fromCharCode(63 + (1 << pos))); // enter sixel mode
+        for (let i = 0; i < 500; i++) {
+            const pos = 6 - Math.round((Math.sin(2 * Math.PI * (i + j) / 500) + 1) * 6 / 2);
+            process.stdout.write(String.fromCharCode(63 + (1 << pos))); // enter sixel mode
+        }
+
+        //process.stdout.write("$"); // next sixel line * 2
+        process.stdout.write("\x1b\\"); // exit sixel mode
+
+        await timers.setTimeout(100);
     }
-
-    process.stdout.write("--"); // next sixel line * 2
 
     process.stdout.write("\x1b\\"); // exit sixel mode
 };
