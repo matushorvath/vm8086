970cf42559ffdccce03549ea82ab68cbf97cfbfa
Matus Horvath <matus.horvath@gmail.com>
2024-09-05 15:19:50 +0200

    Display image using sixels WIP

--- docs/sixels/image.js
+++ docs/sixels/image.js
@@ -1,12 +1,14 @@
 import sharp from 'sharp';
+import rgbquant from 'rgbquant';
 import 'promise.withresolvers/auto';
+import fs from 'node:fs/promises';
 import path from 'node:path';
 
 const keypress = async () => {
     const { resolve, _reject, promise } = Promise.withResolvers();
 
     process.stdin.setRawMode(true);
-    process.stdin.once('data', key => {
+    process.stdin.once('data', () => {
         process.stdin.setRawMode(false);
         process.stdin.pause();
         resolve();
@@ -15,28 +17,15 @@ const keypress = async () => {
     return promise;
 };
 
-// ▀ ▄ █ ▌ ▐ ▖ ▗ ▘ ▙ ▚ ▛ ▜ ▝ ▞ ▟
-
 const saveCursor = () => process.stdout.write('\x1b7');
 const restoreCursor = () => process.stdout.write('\x1b8');
-const setCursor = (row, col) => process.stdout.write(`\x1b[${row};${col}H`);
 const clearDisplay = () => process.stdout.write('\x1b[2J');
 
 const alternateBuffer = () => process.stdout.write('\x1b[?47h');
 const normalBuffer = () => process.stdout.write('\x1b[?47l');
 
-const insertMode = () => process.stdout.write('\x1b[4h');
-const replaceMode = () => process.stdout.write('\x1b[4l');
-
-const autoNewline = () => process.stdout.write('\x1b[20h');
-const normalNewline = () => process.stdout.write('\x1b[20l');
-
 const hideCursor = () => process.stdout.write('\x1b[?25l');
 const showCursor = () => process.stdout.write('\x1b[?25h');
-
-const setForeground = (r, g, b) => process.stdout.write(`\x1b[38;2;${r};${g};${b}m`);
-const setBackground = (r, g, b) => process.stdout.write(`\x1b[48;2;${r};${g};${b}m`);
-const resetColor = () => process.stdout.write('\x1b[0m');
 
 class Frame {
     constructor(cols, rows, data) {
@@ -45,77 +34,127 @@ class Frame {
         this.data = data;
     }
 
-    static CBL = 1;
-    static RBL = 2;
-
     display() {
-        // TODO handle images with size not divisible by CBL, RBL
-        const cblocks = Math.ceil(this.cols / Frame.CBL);
-        const rblocks = Math.ceil(this.rows / Frame.RBL);
+        const palette = this.preparePalette();
+        console.log(palette);
 
-        for (let r = 0; r < rblocks; r++) {
-            for (let c = 0; c < cblocks; c++) {
-                const pixels = this.getBlockPixels(c, r);
-                const { char, foreground, background } = this.getBlockChar(pixels);
+        // TODO handle images with height not divisible by 6
+        // const rowsDiv6 = Math.ceil(this.rows / 6);
 
-                setCursor(r + 1, c + 1);
+        // TODO POC for setPixel based image display - iterate over the real image pixel, setpixel each of them
 
-                setForeground(foreground.r, foreground.g, foreground.b);
-                setBackground(background.r, background.g, background.b);
+        // for (let r6 = 0; r6 < rowsDiv6; r6++) {
+        //     for (let c = 0; c < this.cols; c++) {
+        //         const pixels = this.getSixPixels(c, r6);
+        //         const { char, foreground, background } = this.getBlockChar(pixels);
 
-                process.stdout.write(char);
+        //         setCursor(r + 1, c + 1);
 
-                resetColor();
+        //         setForeground(foreground.r, foreground.g, foreground.b);
+        //         setBackground(background.r, background.g, background.b);
+
+        //         process.stdout.write(char);
+
+        //         resetColor();
+        //     }
+        // }
+    }
+
+    preparePalette() {
+        const palette = {};
+
+        for (let r = 0; r < this.rows; r++) {
+            for (let c = 0; c < this.cols; c++) {
+                const p = this.getPixel(c, r);
+                const key = `${p.r},${p.g},${p.b}`;
+
+                palette[key] = p;
             }
         }
+
+        return Object.values(palette);
     }
 
-    calcIndex(bc, br, dc, dr) {
-        return 3 * ((br * Frame.RBL + dr) * this.cols + (bc * Frame.CBL + dc));
-    }
-
-    getPixel(bc, br, dc, dr) {
-        const index = this.calcIndex(bc, br, dc, dr);
+    getPixel(c, r) {
+        const index = 3 * (r * this.cols + c);
         return { r: this.data[index + 0] ?? 0, g: this.data[index + 1] ?? 0, b: this.data[index + 2] ?? 0 };
     }
 
-    getBlockPixels(c, r) {
-        return [this.getPixel(c, r, 0, 0), this.getPixel(c, r, 0, 1)];
+    getSixPixels(c, r6) {
+        return [...Array(10).keys()].map(i => this.getPixel(c, r6 * 6 + i));
     }
 
-    getBlockChar(pixels) {
-        // TODO determine colors, use the correct character
-        return {
-            char: '▀',
-            foreground: pixels[0],
-            background: pixels[1]
-        };
-    }
+    // setPixel = (c, r, color) => {
+    //     process.stdout.write('\x1bP0;1;0q'); // enter sixel mode
+
+    //     // TODO prepare the palette before, to cover all colors used by the image
+
+    //     process.stdout.write('#0;2;0;0;0'); // set palette
+    //     process.stdout.write('#1;2;100;0;0'); // set palette
+    //     process.stdout.write('#2;2;0;100;0'); // set palette
+    //     process.stdout.write('#3;2;0;0;100'); // set palette
+
+    //     const row = Math.floor(r / 6);
+    //     const pxl = r % 6;
+
+    //     for (let i = 0; i < row; i++) {
+    //         process.stdout.write('-');
+    //     }
+
+    //     for (let i = 0; i < 4; i++) {
+    //         process.stdout.write(`#${i}!${c}?`); // color i, skip x columns
+
+    //         if (i === color) {
+    //             process.stdout.write(String.fromCharCode(63 + (1 << pxl)));
+    //         } else {
+    //             process.stdout.write(String.fromCharCode(63 + ~(1 << pxl)));
+    //         }
+
+    //         process.stdout.write('$');
+    //     }
+
+    //     process.stdout.write('-');
+
+    //     process.stdout.write('\x1b\\'); // exit sixel mode
+    // }
 }
 
 const main = async () => {
-    const cols = 120, rows = 80;
+    const cols = 150, rows = 90;
 
-    const image = await sharp(path.join(process.env.HOME, 'intcode/sammy.png'))
-        .resize(cols, rows, { fit: 'inside' })
-        .toColorspace('srgb');
-    //await image.toFile('delme.png');
-    const buffer = await image.raw().toBuffer({ resolveWithObject: true });
+    const filename = path.join(process.env.HOME, 'intcode/sammy.png');
 
-    const frame = new Frame(buffer.info.width, buffer.info.height, buffer.data);
+    const image = await sharp(filename)
+         .resize(cols, rows, { fit: 'inside' })
+         .toColorspace('rgba');
 
-    hideCursor();
-    saveCursor();
-    alternateBuffer();
-    clearDisplay();
+    const { data, info } = await image.raw().toBuffer({ resolveWithObject: true });
 
-    frame.display();
+    const q = new rgbquant();
+    q.sample(data, cols);
+    const data256 = q.reduce(data);
 
-    await keypress();
+    //const image256 = await sharp({ input: data256, raw: info });
+    //image256.toFile('delme.png');
 
-    normalBuffer();
-    restoreCursor();
-    showCursor();
+    const frame = new Frame(info.width, info.height, data256);
+
+    try {
+        hideCursor();
+        saveCursor();
+        alternateBuffer();
+        clearDisplay();
+
+        process.stdout.write('\x1b[?80h'); // disable sixel scrolling
+
+        frame.display();
+
+        await keypress();
+    } finally {
+        normalBuffer();
+        restoreCursor();
+        showCursor();
+    }
 };
 
 await main();
